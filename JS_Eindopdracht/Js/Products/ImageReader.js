cc7da7d421701f35789daa75b0a398a242342e99
Snowborn <jfa.vanderpoel@student.avans.nl>
2020-03-19 16:27:32 +0100

    begin foto uploaden

--- JS_Eindopdracht/Js/Products/ImageReader.js
+++ JS_Eindopdracht/Js/Products/ImageReader.js
@@ -2,15 +2,12 @@ const canvas = document.getElementById("ImageCanvas");
 
 export class ImageReader {
 
-    LoadImage(){
-
-    }
-
     clearInputFile(f) {
         if (f.value) {
             try {
-                f.value = ''; //for IE11, latest Chrome/Firefox/Opera...
+                f.value = '';
             } catch (err) {
+                alert("Sorry, er is iets mis gegaan!")
             }
         }
     }
@@ -23,7 +20,19 @@ export class ImageReader {
         }
         let fReader = new FileReader();
         fReader.onload = (function () {
+            return function (e) {
+                canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+                let buffer = new Image();
+                buffer.src = e.target.result;
+                buffer.onload = function(){
+                    let sizer = Math.min((500/buffer.width),(500/buffer.height));
+                    canvas.width = 500;
+                    canvas.height = 500;
 
+                    canvas.getContext("2d")
+                        .drawImage(buffer, 0, 0, buffer.width, buffer.height, 0, 0, buffer.width*sizer,  buffer.height*sizer);
+                }
+            }
         })
     }
 }
